54f866fac88a9488b6061ba43e49eafcd9f76118
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-01-07 19:33:20 +0200

    initialize Database class

---
+++ src/server/DataAccessLayer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
+/**
+ *
+ * @author ahmed
+ */
+public class DataAccessLayer {
+    
+    private static DataAccessLayer DataRefaerence;
+    private Connection con;
+    private ResultSet rs ;
+    private PreparedStatement pst;
+    
+    public synchronized ResultSet getResultSet(){
+        return rs;
+    }
+    
+    private  DataAccessLayer() throws SQLException{
+         DriverManager.registerDriver(new ClientDriver());
+         con = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe","root","root");
+    }
+
+    public synchronized static DataAccessLayer startDataBase() throws SQLException {
+        if(DataRefaerence == null){
+            DataRefaerence = new DataAccessLayer();
+        }
+        return DataRefaerence;
+    }
+    
+}
