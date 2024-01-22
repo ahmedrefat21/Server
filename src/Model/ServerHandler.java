155de68e0963035a362c27711bea37e4500ef932
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-01-22 16:41:44 +0200

    add fun startServer

--- src/Model/ServerHandler.java
+++ src/Model/ServerHandler.java
@@ -5,10 +5,54 @@
  */
 package Model;
 
+import dao.DAO;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ahmed
  */
 public class ServerHandler {
+    
+    private static ServerHandler server;
+    public DAO database ;
+    private ServerSocket serverSocket ;
+    private Socket socket ;
+    private Thread thread;
+
+    
+    private ServerHandler(){}
+    
+    public static ServerHandler getServer(){
+        if(server == null){
+            server = new ServerHandler();
+        }
+        return server;
+    }
+    
+    
+    private void startServer(){
+        try {
+            serverSocket = new ServerSocket(5005);
+            thread = new Thread(() -> {
+                while(true){
+                    try {
+                        socket = serverSocket.accept();
+                        new PlayerHandler(socket); 
+                    }catch (IOException ex) {
+                        ex.printStackTrace();
+                    }    
+                }
+            });
+            thread.start();
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     
 }
