e63f5f4a0959ace0de850a304ecbb6a6bf5723df
MarimYacout <marim.yacout@gmail.com>
2024-01-22 16:20:20 +0200

    (set not busy) function in DAO

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
-/**
- *
- * @author ahmed
- */
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
+
 public class DAO {
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public synchronized void setNotBusy (String email){
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToy", "root", "root");
+            PreparedStatement ps = connection.prepareStatement("update player set isPlaying = false where email = ?");
+            ps.setString(1, email);
+            ps.executeUpdate();
+            ResultSet();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
+
