010f6834cf84e0ec7d9ca94408250988d500af7d
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-01-22 16:59:23 +0200

    add fun login

--- src/Model/PlayerHandler.java
+++ src/Model/PlayerHandler.java
@@ -139,4 +139,35 @@ public class PlayerHandler extends Thread implements Initializable {
        }
     }
     
+    private void login(){
+        email = token.nextToken();
+        String password = token.nextToken();
+        String data;
+        int score;
+        try{
+            data = server.checkIslogin(email, password);
+            if(data.equals("Logged in successfully")){
+                score = server.retriveScore(email);
+                username = server.getPlayerUserName(email);
+                server.login(email, password);
+                ps.println(data +"###" + score);
+                ps.println(username+"###"+email+"###"+score); // send data to registerController
+                login = true;
+                activeUsers.add(this);
+            }else if(data.equals("This Email is alreay sign-in")){
+                System.out.println("alread in connected");
+                ps.println(data +"###");
+            }else if(data.equals("Email is incorrect")){
+                ps.println(data +"###");
+            }else if(data.equals("Password is incorrect")){
+                ps.println(data +"###");
+            }else if(data.equals("Connection issue, please try again later")){
+                ps.println(data +"###");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        token = null;
+   }
+    
 }
