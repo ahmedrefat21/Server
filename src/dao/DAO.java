57d2d9217a4e0ef4e2b418e339f23b5f913eed2c
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-01-22 16:31:43 +0200

    add fun to check if player logged or not

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -5,10 +5,55 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
 /**
  *
  * @author ahmed
  */
 public class DAO {
+    
+    private static DAO db;
+    private Connection connection;
+    private ResultSet result ;
+    private PreparedStatement pst;
+    
+    public synchronized ResultSet getResultSet(){
+        return result;
+    }
+    
+    private  DAO() throws SQLException{
+         DriverManager.registerDriver(new ClientDriver());
+         connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToe","root","root");
+    }
+    
+    
+    public synchronized String checkisalreadyloginIn(String email, String password){
+        if(!checkIsOnline(email)){
+            try { 
+                PreparedStatement pst = connection.prepareStatement("select * from player where email = ? ");
+                pst.setString(1, email);
+                result = pst.executeQuery();
+                if(result.next()){
+                    if(password.equals(result.getString(4))){
+                        return "Logged in successfully";
+                    }
+                    return "Password is incorrect";
+                }
+                return "Email is incorrect";
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                return "Connection issue, please try again later";
+            }
+        }else{
+            return "This Email is alreay sign-in";  
+        }
+              
+    }
     
 }
