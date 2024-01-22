ff3abec609bcfca99ef33153e3619568b7135357
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-22 16:57:35 +0200

    getOfflineUsers Function

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -16,7 +16,6 @@ import org.apache.derby.jdbc.ClientDriver;
 public class DAO {
     
     public synchronized ResultSet getOnlinePlayers( ){
-        
         try {
             DriverManager.registerDriver(new ClientDriver());
             connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToy", "root", "root");
@@ -28,9 +27,37 @@ public class DAO {
         }
         
     } 
-    
-    
-    public synchronized ResultSet getOfflinePlayers( ){   
+        public synchronized int getOfflineUsers() {
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToy", "root", "root");
+            PreparedStatement ps = connection.prepareStatement("select count(*) from player where isactive = false", ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet r = ps.executeQuery();
+            if (r.next()) {
+                return r.getInt(1);
+            } else {
+                return -1;
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return -1;
+    }   
+        
+        
+        public synchronized ResultSet getActivePlayersChart(){
+        try {
+            this.pst =connection.prepareStatement("SELECT COUNT(*) AS true_count FROM PLAYER WHERE isactive = true",ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY  );
+            return pst.executeQuery(); 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            System.out.println("catch getactive");
+            return null;
+        }
+        
+    }
+        
+        public synchronized ResultSet getOfflinePlayers( ){   
         try {
             this.pst =connection.prepareStatement("SELECT COUNT(*) AS false_count FROM PLAYER WHERE isactive = false",ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY  );
             return pst.executeQuery(); 
@@ -42,18 +69,8 @@ public class DAO {
         
     }
     
-    
-    public synchronized ResultSet getActivePlayersChart(){
-        try {
-            this.pst =connection.prepareStatement("SELECT COUNT(*) AS true_count FROM PLAYER WHERE isactive = true",ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY  );
-            return pst.executeQuery(); 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println("catch getactive");
-            return null;
-        }
-        
-    }
+   
+
     
     
     
