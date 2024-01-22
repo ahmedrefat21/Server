33fd4a791fb14aa2623281fe63b2d2525617997a
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-22 16:37:17 +0200

    startChart Function to retrieve players data from DB

--- src/Controller/ServerMainPageController.java
+++ src/Controller/ServerMainPageController.java
@@ -1,24 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
 
 import Model.Server;
 import Model.ServerHandler;
 import java.net.URL;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.image.ImageView;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.NumberAxis;
 
 /**
  * FXML Controller class
@@ -30,34 +29,22 @@ public class ServerMainPageController implements Initializable {
     ResultSet chartData;
     public static boolean serverState ;
     int trueCount, falseCount;
-//    @FXML
-//    private Button startBtn;
-//    @FXML
-//    private BarChart<?, ?> barChart;
-//    @FXML
-//    private ImageView serverStateImage;
-//    @FXML
-//    private Button startBtn1;
-//    @FXML
-//    private Button startChart;
-//    @FXML
-//    private ImageView img;
-        @FXML
-        private Button startBtn;
-        @FXML
-        private CategoryAxis xAxis;
-        @FXML
-        private NumberAxis yAxis;
-        @FXML
-        private Button startChart;
-        @FXML
-        private ImageView img;
-        @FXML
-        private BarChart<String, Number> barChart;
-        @FXML
-        private ImageView serverStateImage;
-        @FXML
-        private Button startBtn1;
+    @FXML
+     private Button startBtn;
+    @FXML
+     private CategoryAxis xAxis;
+    @FXML
+     private NumberAxis yAxis;
+    @FXML
+     private Button startChart;
+    @FXML
+     private ImageView img;
+    @FXML
+     private BarChart<String, Number> barChart;
+    @FXML
+     private ImageView serverStateImage;
+    @FXML
+     private Button startBtn1;
     
 
     @Override
@@ -77,6 +64,7 @@ public class ServerMainPageController implements Initializable {
                StartChart();              
             }catch(SQLException e){
                 System.out.println("wrong in the connection");
+                System.out.println(e);
                 serverState = !serverState;
             }
 
@@ -99,10 +87,30 @@ public class ServerMainPageController implements Initializable {
             }
         }
 
-    @FXML
-    private void StartChart(ActionEvent event) {
+         
+     
+   private void StartChart() {
+    try {
+
+        ResultSet rsTrue = server.getActivePlayersChart();
+         trueCount = 0;
+           if (rsTrue.next()) {
+            trueCount = rsTrue.getInt("true_count");
+            }
+
+        ResultSet rsFalse = server.getOfflinePlayers();
+         falseCount = 0;
+           if (rsFalse.next()) {
+            falseCount = rsFalse.getInt("false_count");
+            }
+    } catch (SQLException ex) {
+        System.out.println(ex);
     }
 
+   }
+         
+         
+         
     @FXML
     private void mouseEntered(MouseEvent event) {
     }
