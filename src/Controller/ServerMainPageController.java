7b2de645e620e2e62793e4941ba4fd67c9408127
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-22 16:08:13 +0200

    start server function in server controller
    created controller of the severScreenFXML

---
+++ src/Controller/ServerMainPageController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Server;
+import Model.ServerHandler;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kimo Store
+ */
+public class ServerMainPageController implements Initializable {
+    ServerHandler server;
+    ResultSet chartData;
+    public static boolean serverState ;
+    int trueCount, falseCount;
+//    @FXML
+//    private Button startBtn;
+//    @FXML
+//    private BarChart<?, ?> barChart;
+//    @FXML
+//    private ImageView serverStateImage;
+//    @FXML
+//    private Button startBtn1;
+//    @FXML
+//    private Button startChart;
+//    @FXML
+//    private ImageView img;
+        @FXML
+        private Button startBtn;
+        @FXML
+        private CategoryAxis xAxis;
+        @FXML
+        private NumberAxis yAxis;
+        @FXML
+        private Button startChart;
+        @FXML
+        private ImageView img;
+        @FXML
+        private BarChart<String, Number> barChart;
+        @FXML
+        private ImageView serverStateImage;
+        @FXML
+        private Button startBtn1;
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void startServer(ActionEvent event) {
+    }
+
+    @FXML
+    private void shutDown(ActionEvent event) {
+    }
+
+    @FXML
+    private void StartChart(ActionEvent event) {
+    }
+
+    @FXML
+    private void mouseEntered(MouseEvent event) {
+    }
+    
+}
