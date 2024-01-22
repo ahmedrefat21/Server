b3495e4882e15b4d4cdb40a86b6356468f762e94
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-22 16:28:43 +0200

    getOnlinePlayers Function

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -4,11 +4,32 @@
  * and open the template in the editor.
  */
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.derby.jdbc.ClientDriver;
 
-/**
- *
- * @author ahmed
- */
 public class DAO {
+    
+    public synchronized ResultSet getOnlinePlayers( ){
+        
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToy", "root", "root");
+            PreparedStatement ps =connection.prepareStatement("Select * from player where isactive = true ",ResultSet.TYPE_SCROLL_SENSITIVE ,ResultSet.CONCUR_READ_ONLY  );
+            return ps.executeQuery(); 
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        
+    }   
+    
+    
+    
     
 }
