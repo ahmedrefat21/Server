48421fc6a6fde952d87438b88f544bd3f4ca51b0
MarimYacout <marim.yacout@gmail.com>
2024-01-22 16:36:09 +0200

    PlayerHandler function in PlayerHandler class

--- src/Model/PlayerHandler.java
+++ src/Model/PlayerHandler.java
@@ -5,10 +5,40 @@
  */
 package Model;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ahmed
  */
 public class PlayerHandler {
     
+    
+    
+    
+    
+    
+    
+    
+    public PlayerHandler(Socket socket){
+       server = ServerHandler.getServer();
+       try {
+            dis = new DataInputStream(socket.getInputStream());
+            ps = new PrintStream(socket.getOutputStream());
+            mySocket = socket;
+            this.start();
+       }catch (IOException ex) {
+            ex.printStackTrace();
+           try {
+               socket.close();
+           } catch (IOException ex1) {
+               Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex1);
+           }
+       }
+   }
 }
