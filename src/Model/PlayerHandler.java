1a0c065ae084f7dc027605ec36e9cea987a763c4
ahmedrefat21 <105991645+ahmedrefat21@users.noreply.github.com>
2024-01-22 15:01:45 +0200

    SetUp Server Classes

--- src/Model/PlayerHandler.java
+++ src/Model/PlayerHandler.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package server;
+package Model;
 
 /**
  *
  * @author ahmed
  */
-public class DataBase {
+public class PlayerHandler {
     
 }
